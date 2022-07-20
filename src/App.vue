474c7ed076f09d467733d3d4c18144ac9b6a6df6
Armani Appolon <ArmaniAppolon@gmail.com>
2022-07-20 15:37:22 -0400

    update to video

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script setup>
 import HelloWorld from "@/components/HelloWorld.vue";
+import VideoPlayer from "./components/VideoPlayer.vue";
 </script>
 
 <template>
   <div id="app">
+    <HelloWorld msg="Rick & Morty Characters" />
     <VideoPlayer
       style="width: 800px; margin: 0 auto"
       src="../public/Dog on the beach.mp4"
     />
-    <HelloWorld msg="Rick & Morty Characters" />
   </div>
 </template>
