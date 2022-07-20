895a4805b0ed4c6b226887d05490422d5d422fd0
Armani Appolon <ArmaniAppolon@gmail.com>
2022-07-20 14:49:52 -0400

    end of 8 commit, and end of project

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,8 +1,18 @@
 <script setup>
 import { ref } from "vue";
+import { useGlobalEvent } from "@/composables/useGlobalEvent";
+
 defineProps({
   src: String,
 });
+
+useGlobalEvent("keypress", (e) => {
+  console.log(e);
+  if (e.charCode !== 32) return;
+  e.preventDefault();
+  video.value.paused ? video.value.play() : video.value.pause();
+});
+
 const video = ref(null);
 </script>
 
