891f865659da8f4914be0630b560d0e1b4a7199e
Armani Appolon <ArmaniAppolon@gmail.com>
2022-07-20 15:39:10 -0400

    updated the url to use the public proper

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,6 @@ import VideoPlayer from "./components/VideoPlayer.vue";
 <template>
   <div id="app">
     <HelloWorld msg="Rick & Morty Characters" />
-    <VideoPlayer
-      style="width: 800px; margin: 0 auto"
-      src="../public/Dog on the beach.mp4"
-    />
+    <VideoPlayer style="width: 800px; margin: 0 auto" src="/Dog on the beach.mp4" />
   </div>
 </template>
